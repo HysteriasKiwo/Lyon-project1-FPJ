c3f22007b7af6201dc7637f5fb4ea13fc60733ea
hysteriaskiwo <cerynna@gmail.com>
2017-09-11 16:20:46 +0200

    add all structure of BUG BURGER

---
+++ FAQ.php
@@ -0,0 +1,33 @@
+
+            <h1>QUESTIONS FRÉQUENTES</h1>
+            <h2><small>Nous répondons ici aux questions les plus fréquemment posées sur nos produits et tout autour de notre site web. Peut-être que vous trouverez déjà une réponse à vos questions.</small></h2>
+
+
+        <h3>Notre nouveau site web BUG BURGER®</h3><br />
+
+
+                <h3 class="panel-title">Quelles sont les nouveautés sur notre nouveau site web BUG BURGER® ?</h3>
+
+                Nos recettes n'ont pas changé. Mais avec ce site, vous allez pouvoir les redécouvrir et nous trouver plus facilement.
+
+                <h3 class="panel-title">Quelles informations est-ce que j'obtiens sur vos produits ?</h3>
+
+                Notre nouvelle vue d’ensemble des produits est encore plus détaillée : nous y avons ajouté de nombreuses informations intéressantes sur les ingrédients et la préparation de nos produits. Vous pouvez savoir ici d’où proviennent nos ingrédients et à quelle température nous grillons votre burger, mais également des informations sur les valeurs nutritionnelles et les allergènes.
+            Peut-on manger des insectes trouvés dans les jardins ou les forêts ?Comme pour les végétaux et les champignons, toutes les espèces d’insectes ne sont pas comestibles. Hormis pour les spécialistes des insectes, il n’est donc pas conseillé de récolter soi-même des insectes à des fins alimentaires. De plus, les insectes collectés dans la nature peuvent avoir été en contact avec des pesticides ou autres composés chimiques.
+
+            Est-ce que les végétariens peuvent manger des insectes ?La consommation d’insectes semble être proche des motivations des végétariens, cependant les différents régimes végétariens ne prévoient pas de consommation d’insectes pour le moment.
+            Manger des insectes est aussi incompatible avec le régime végétalien qui exclue toute protéine d’origine animale.
+
+            Quel est l’insecte le plus adapté à une première dégustation ?Les grillons et les vers de farine sont des espèces faciles à préparer et à cuisiner. Ils ont aussi l’avantage d’être nutritif et délicieux.
+
+            Est-ce que tout se mange chez l’insecte ?Le vers de farine et le grillon peuvent se manger entièrement. Pour les criquets adultes, certaines personnes préfèrent les consommer sans les ailes et les grandes pattes postérieures.
+
+            Quelle est la différence entre entomologie et entomophagie ?L’entomologie est le terme utilisé pour désigner l’étude des insectes alors que l’entomophagie désigne la consommation d’insectes par les êtres humains. On peut ainsi être entomologiste sans être entomophagiste ou vice et versa.
+
+            Qu’est ce qu’un ver de farine ?Le ver de farine est la larve d’une espèce d’insecte appelée le Ténébrion Meunier. On utilise le terme ver bien qu’il n’appartienne pas au groupe des vers comprenant notamment les vers de terre. Il est capable de se développer dans des denrées stockées comme la farine d’où son nom de Meunier. A une certaine époque, on les trouvait fréquemment dans les boulangeries où était stockée de la farine en grande quantité.
+
+            Les insectes se mangent-ils vivants ou cuits ?Pour des raisons sanitaires, les insectes se consomment généralement après cuisson même si certains initiés les consomment vivants. Attention, aucun insecte trouvé mort dans la nature ne doit être consommé.
+
+            Puis-je en faire manger à mes enfants ?Les enfants peuvent aussi déguster des insectes comestibles. Les insectes contiennent de nombreux éléments indispensables à la croissance et bons pour la santé.
+
+
---
+++ carte.php
@@ -0,0 +1,96 @@
+
+
+    <h1>Nos Menus</h1>
+
+    <hr>
+
+    <div class="row">
+        <div class="col-xs-12">
+            <div class="media">
+
+                <div class="row">
+                    <div class="col-xs-3">
+                        <div class="media-left">
+                            <img class="media-object img-thumbnail" src="http://via.placeholder.com/200x200" alt="...">
+                        </div>
+                    </div>
+
+                    <div class="col-xs-9">
+                        <div class="media-body">
+                            <h2 class="media-heading">Burger 1</h2>
+                            <p class="text-justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat.</p>
+                        </div>
+                    </div>
+                </div>
+
+                <hr>
+
+                <div class="row">
+                    <div class="col-xs-9">
+                        <div class="media-body">
+                            <h2 class="media-heading">Burger 2</h2>
+                            <p class="text-justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat.</p>
+                        </div>
+                    </div>
+
+                    <div class="col-xs-3">
+                        <div class="media-right pull-right">
+                            <img class="media-object img-thumbnail" src="http://via.placeholder.com/200x200" alt="...">
+                        </div>
+                    </div>
+                </div>
+
+                <hr>
+
+                <div class="row">
+                    <div class="col-xs-3">
+                        <div class="media-left">
+                            <img class="media-object img-thumbnail" src="http://via.placeholder.com/200x200" alt="...">
+                        </div>
+                    </div>
+
+                    <div class="col-xs-9">
+                        <div class="media-body">
+                            <h2 class="media-heading">Burger 3</h2>
+                            <p class="text-justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat.</p>
+                        </div>
+                    </div>
+                </div>
+
+                <hr>
+
+                <div class="row">
+                    <div class="col-xs-9">
+                        <div class="media-body">
+                            <h2 class="media-heading">Burger 4</h2>
+                            <p class="text-justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat.</p>
+                        </div>
+                    </div>
+
+                    <div class="col-xs-3">
+                        <div class="media-right pull-right">
+                            <img class="media-object img-thumbnail" src="http://via.placeholder.com/200x200" alt="...">
+                        </div>
+                    </div>
+                </div>
+
+                <hr>
+
+                <div class="row">
+                    <div class="col-xs-3">
+                        <div class="media-left">
+                            <img class="media-object img-thumbnail" src="http://via.placeholder.com/200x200" alt="...">
+                        </div>
+                    </div>
+
+                    <div class="col-xs-9">
+                        <div class="media-body">
+                            <h2 class="media-heading">Burger 5</h2>
+                            <p class="text-justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat.</p>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
