393b67102067547e5d4f4af0f2b66748712d0117
pibouv <pierre@prieuretaluyers.fr>
2017-09-19 14:48:34 +0200

    Modification FAQ

--- FAQ.php
+++ FAQ.php
@@ -1,34 +1,43 @@
 
-            <h1>QUESTIONS FRÉQUENTES</h1>
-            <h2><small>Nous répondons ici aux questions les plus fréquemment posées sur nos produits et tout autour de notre site web. Peut-être que vous trouverez déjà une réponse à vos questions.</small></h2>
+<h1>QUESTIONS FRÉQUENTES</h1>
+<h3 class="h3">Nous répondons ici aux questions les plus fréquemment posées sur nos produits et tout autour de notre site web. Peut-être que vous trouverez déjà une réponse à vos questions.</h3><br />
 
 
-        <h3>Notre nouveau site web BUG BURGER®</h3><br />
+<h2>Quelles sont les nouveautés sur notre nouveau site web BUG BURGER® ?</h2>
 
+<p class="plat">Nos recettes n'ont pas changé. Mais avec ce site, vous allez pouvoir les redécouvrir et nous trouver plus facilement.
+Notre nouvelle vue d’ensemble des produits est encore plus détaillée : nous y avons ajouté de nombreuses informations intéressantes sur les ingrédients et la préparation de nos produits. Vous pouvez savoir ici d’où proviennent nos ingrédients et à quelle température nous grillons votre burger, mais également des informations sur les valeurs nutritionnelles et les allergènes.</p><br />
 
-                <h3 class="panel-title">Quelles sont les nouveautés sur notre nouveau site web BUG BURGER® ?</h3>
+<h2>Peut-on manger des insectes trouvés dans les jardins ou les forêts ?</h2>
 
-                Nos recettes n'ont pas changé. Mais avec ce site, vous allez pouvoir les redécouvrir et nous trouver plus facilement.
+<p class="plat">Comme pour les végétaux et les champignons, toutes les espèces d’insectes ne sont pas comestibles. Hormis pour les spécialistes des insectes, il n’est donc pas conseillé de récolter soi-même des insectes à des fins alimentaires.
+    De plus, les insectes collectés dans la nature peuvent avoir été en contact avec des pesticides ou autres composés chimiques.</p><br />
 
-                <h3 class="panel-title">Quelles informations est-ce que j'obtiens sur vos produits ?</h3>
+<h2>Est-ce que les végétariens peuvent manger des insectes ?</h2>
 
-                <!--Notre nouvelle vue d’ensemble des produits est encore plus détaillée : nous y avons ajouté de nombreuses informations intéressantes sur les ingrédients et la préparation de nos produits. Vous pouvez savoir ici d’où proviennent nos ingrédients et à quelle température nous grillons votre burger, mais également des informations sur les valeurs nutritionnelles et les allergènes.
-            Peut-on manger des insectes trouvés dans les jardins ou les forêts ?Comme pour les végétaux et les champignons, toutes les espèces d’insectes ne sont pas comestibles. Hormis pour les spécialistes des insectes, il n’est donc pas conseillé de récolter soi-même des insectes à des fins alimentaires. De plus, les insectes collectés dans la nature peuvent avoir été en contact avec des pesticides ou autres composés chimiques.
+<p class="plat">La consommation d’insectes semble être proche des motivations des végétariens, cependant les différents régimes végétariens ne prévoient pas de consommation d’insectes pour le moment.
+Manger des insectes est aussi incompatible avec le régime végétalien qui exclue toute protéine d’origine animale.</p><br />
 
-            Est-ce que les végétariens peuvent manger des insectes ?La consommation d’insectes semble être proche des motivations des végétariens, cependant les différents régimes végétariens ne prévoient pas de consommation d’insectes pour le moment.
-            Manger des insectes est aussi incompatible avec le régime végétalien qui exclue toute protéine d’origine animale.
+<h2>Quel est l’insecte le plus adapté à une première dégustation ?</h2>
 
-            Quel est l’insecte le plus adapté à une première dégustation ?Les grillons et les vers de farine sont des espèces faciles à préparer et à cuisiner. Ils ont aussi l’avantage d’être nutritif et délicieux.
+<p class="plat">Les grillons et les vers de farine sont des espèces faciles à préparer et à cuisiner. Ils ont aussi l’avantage d’être nutritif et délicieux.</p><br />
 
-            Est-ce que tout se mange chez l’insecte ?Le vers de farine et le grillon peuvent se manger entièrement. Pour les criquets adultes, certaines personnes préfèrent les consommer sans les ailes et les grandes pattes postérieures.
+<h2>Est-ce que tout se mange chez l’insecte ?</h2>
 
-            Quelle est la différence entre entomologie et entomophagie ?L’entomologie est le terme utilisé pour désigner l’étude des insectes alors que l’entomophagie désigne la consommation d’insectes par les êtres humains. On peut ainsi être entomologiste sans être entomophagiste ou vice et versa.
+<p class="plat">Le vers de farine et le grillon peuvent se manger entièrement. Pour les criquets adultes, certaines personnes préfèrent les consommer sans les ailes et les grandes pattes postérieures.</p><br />
 
-            Qu’est ce qu’un ver de farine ?Le ver de farine est la larve d’une espèce d’insecte appelée le Ténébrion Meunier. On utilise le terme ver bien qu’il n’appartienne pas au groupe des vers comprenant notamment les vers de terre. Il est capable de se développer dans des denrées stockées comme la farine d’où son nom de Meunier. A une certaine époque, on les trouvait fréquemment dans les boulangeries où était stockée de la farine en grande quantité.
+<h2>Quelle est la différence entre entomologie et entomophagie ?</h2>
 
-            Les insectes se mangent-ils vivants ou cuits ?Pour des raisons sanitaires, les insectes se consomment généralement après cuisson même si certains initiés les consomment vivants. Attention, aucun insecte trouvé mort dans la nature ne doit être consommé.
+<p class="plat">L’entomologie est le terme utilisé pour désigner l’étude des insectes alors que l’entomophagie désigne la consommation d’insectes par les êtres humains. On peut ainsi être entomologiste sans être entomophagiste ou vice et versa.</p><br />
 
-            Puis-je en faire manger à mes enfants ?Les enfants peuvent aussi déguster des insectes comestibles. Les insectes contiennent de nombreux éléments indispensables à la croissance et bons pour la santé.
+<h2>Qu’est ce qu’un ver de farine ?</h2>
 
+<p class="plat">Le ver de farine est la larve d’une espèce d’insecte appelée le Ténébrion Meunier. On utilise le terme ver bien qu’il n’appartienne pas au groupe des vers comprenant notamment les vers de terre. Il est capable de se développer dans des denrées stockées comme la farine d’où son nom de Meunier. A une certaine époque, on les trouvait fréquemment dans les boulangeries où était stockée de la farine en grande quantité.</p><br />
 
--->
+<h2>Les insectes se mangent-ils vivants ou cuits ?</h2>
+
+<p class="plat">Pour des raisons sanitaires, les insectes se consomment généralement après cuisson même si certains initiés les consomment vivants. Attention, aucun insecte trouvé mort dans la nature ne doit être consommé.</p><br />
+
+<h2>Puis-je en faire manger à mes enfants ?</h2>
+
+<p class="plat">Les enfants peuvent aussi déguster des insectes comestibles. Les insectes contiennent de nombreux éléments indispensables à la croissance et bons pour la santé.</p><br />
